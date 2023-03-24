2917fbe6eaf3c445d449ec3e561b30301ff3b70f
Harsha ShivaKumar <54899429+HarshaShivaKumar7@users.noreply.github.com>
2023-03-24 19:14:22 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 let slideIndex = 1;
 
-currentSlide(slideIndex);
+showSlides(slideIndex);
 
-function numberSlides(n) {
-  currentSlide((slideIndex = slideIndex + n));
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
@@ -16,4 +20,13 @@ function currentSlide(n) {
   if (n < 1) {
     slideIndex = slides.length;
   }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000);
 }
